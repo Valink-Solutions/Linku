6f746525707b7b75095c439b9684d40b19e5a797
pixl <jakewjevans@gmail.com>
2022-11-11 01:37:38 -0500

    removed app_secret

--- src/api/core/settings.py
+++ src/api/core/settings.py
@@ -13,6 +13,3 @@ except ImportError:
 FAVICON_API = "https://favicongrabber.com/api/grab"
 
 APP_URL = os.getenv('DETA_SPACE_APP_HOSTNAME')
-
-JWT_SECRET = os.getenv("APP_SECRET", "489F5CE81A2BEA4E93B97CF4A2857")
-JWT_ALGORITHM = "HS256"
